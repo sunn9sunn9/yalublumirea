208e2b2377ed12630b4b9125760649f7cfa44d59
sunn9sunn9 <134864336+sunn9sunn9@users.noreply.github.com>
2023-05-28 20:42:56 +0300

    Create cone.h

---
+++ praktika6/zadanie2/cone.h
@@ -0,0 +1,55 @@
+#ifndef INDUSTRIAL_PROGRAMMENT_PRACTIC_CONE_H
+#define INDUSTRIAL_PROGRAMMENT_PRACTIC_CONE_H
+
+#include <iostream>
+
+class Cone {
+public:
+    Cone();
+
+    Cone(float x, float y, float z, float r, float h);
+
+    virtual ~Cone() = default;
+
+    virtual void input();
+
+    virtual void output() const;
+
+    [[nodiscard]] virtual float surfaceArea() const;
+
+    [[nodiscard]] virtual float volume() const;
+
+protected:
+    float x, y, z;
+    float r;
+    float h;
+};
+
+class TruncatedCone : public Cone {
+public:
+    TruncatedCone();
+
+    TruncatedCone(float x, float y, float z, float r1, float r2, float h);
+
+    virtual void input() override;
+
+    virtual void output() const override;
+
+    [[nodiscard]] virtual float surfaceArea() const override;
+
+    [[nodiscard]] virtual float volume() const override;
+
+    bool operator==(const TruncatedCone &other) const;
+
+    bool operator!=(const TruncatedCone &other) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const TruncatedCone &cone);
+
+    friend std::istream &operator>>(std::istream &is, TruncatedCone &cone);
+
+protected:
+    float r2;
+};
+
+
+#endif //INDUSTRIAL_PROGRAMMENT_PRACTIC_CONE_H
