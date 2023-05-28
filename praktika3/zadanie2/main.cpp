7ce393e807d71d6fed83871d4f662d151b223256
sunn9sunn9 <134864336+sunn9sunn9@users.noreply.github.com>
2023-05-28 20:37:25 +0300

    Create main.cpp

---
+++ praktika3/zadanie2/main.cpp
@@ -0,0 +1,30 @@
+#include "rational.h"
+#include "iostream"
+
+
+int main() {
+    system("chcp 65001");
+    int n;
+    std::cout << "Введите размер массива: ";
+    std::cin >> n;
+
+    rational *A = new rational;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+
+        std::cout << "\nВведите числитель и знаменатель " << i + 1 << " дроби через пробел:\n";
+        std::cin >> a >> b;
+
+        A[i].set(a, b);
+    }
+
+    std::cout << "Введенные дроби:\n";
+
+    for (int i = 0; i < n; i++) {
+        A[i].show();
+        std::cout << '\n';
+    }
+
+    delete[] A;
+}
