fbba43a158364a7f8d5d2409ca60e6b82d73ec70
sunn9sunn9 <134864336+sunn9sunn9@users.noreply.github.com>
2023-05-28 20:15:37 +0300

    Create treuglonik.cpp

---
+++ praktika2/zadanie1/treuglonik.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#include "Triangle.h"
+
+bool Triangle::exst_tr() {
+    return ((a + b > c) && (b + c > a) && (a + c > b));
+}
+
+void Triangle::set(double a1, double b1, double c1) {
+    a = a1;
+    b = b1;
+    c = c1;
+}
+
+void Triangle::show() {
+    cout << "первая сторона: " << a << endl;
+    cout << "вторая сторона: " << b << endl;
+    cout << "третья сторона: " << c << endl;
+}
+
+double Triangle::perimetr() {
+    double p = a + b + c;
+    return p;
+}
+
+double Triangle::square() {
+    double p = (a + b + c) / 2;
+    double s = sqrt(p * (p - a) * (p - b) * (p - c));
+    return s;
+}
