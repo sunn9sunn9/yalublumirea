4bea45f9eca170e73942c5d280c1c66ceecc45ca
sunn9sunn9 <134864336+sunn9sunn9@users.noreply.github.com>
2023-05-28 20:35:47 +0300

    Create eq2.h

---
+++ praktika3/zadanie1/eq2.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+class eq2 {
+    double a, b, c;
+    double D;
+
+public:
+    eq2(double a1, double b1, double c1);
+
+    void set(double a1, double b1, double c1);
+
+    double find_X();
+
+    double find_Y(double x1);
+
+    double get_a();
+
+    double get_b();
+
+    double get_c();
+
+    void print();
+};
+
+eq2 operator+(eq2 A, eq2 B);
