99d48a09dfd32cb50f25045380c4db6f0d7b52b5
sunn9sunn9 <134864336+sunn9sunn9@users.noreply.github.com>
2023-05-28 23:22:42 +0300

    Create type1.cpp

---
+++ praktika7/zadanie1/type1.cpp
@@ -0,0 +1,10 @@
+#include "type1.h"
+#include "iostream"
+
+void type1::Get_answer() {
+    std::cout << "The answer is any value of X"<< std::endl;
+}
+
+void type1::show() {
+    std::cout << "Equation 0 = 0" << std::endl;
+}
